4b2b48571a008868741e7ecbf961cefd42ea584a
mcwilk <mcwilk@wp.pl>
2024-01-09 20:09:18 +0100

    Add timeout to click

--- Basics/test_codegen.py
+++ Basics/test_codegen.py
@@ -16,11 +16,11 @@ def run(playwright: Playwright) -> None:
     # Go to specific WWW address
     page.goto("https://antoogle.testoneo.com/")
 
-
     # Find and act on webelements
     page.get_by_placeholder("Search phrase").click()
     page.get_by_placeholder("Search phrase").fill(input_phrase)
-    page.get_by_role("button", name="Search!").click()
+
+    page.get_by_role("button", name="Search!").click(timeout=2000)
 
     assert page.inner_text("#item0") == input_phrase, f'Actual value \"{page.inner_text("#item0")}\" not equals to ' \
                                                       f'expected value: \"{input_phrase}\"'
