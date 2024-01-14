368c1716ddf09945f3908c41e7ce15949c96ac72
mcwilk <mcwilk@wp.pl>
2024-01-14 13:35:58 +0100

    Add Excel reader and XLSX test data

--- FirstAppTestingWithPOP/tests/test_hotel_search.py
+++ FirstAppTestingWithPOP/tests/test_hotel_search.py
@@ -3,6 +3,7 @@ import pytest
 
 from FirstAppTestingWithPOP.pages.search_hotel_page import SearchHotelPage
 from FirstAppTestingWithPOP.pages.search_results_page import SearchResultsPage
+from FirstAppTestingWithPOP.utils.read_excel import ExcelReader
 
 
 @pytest.mark.usefixtures("setup")
@@ -10,12 +11,13 @@ class TestHotelSearch:
 
     @allure.title("Test method: test_hotel_search")
     @allure.description("Test checking if 'search' option works as expected")
-    def test_hotel_search(self, setup):
+    @pytest.mark.parametrize("data", ExcelReader.get_data())
+    def test_hotel_search(self, setup, data):
         page = setup
 
         search_hotel_page = SearchHotelPage(page)
         search_hotel_page.set_city("Dubai")
-        search_hotel_page.set_date_range("09/09/2019", "09/13/2019")
+        search_hotel_page.set_date_range(data.check_in, data.check_out)
         search_hotel_page.set_travellers("4", "3")
         search_hotel_page.perform_search()
 
